7d403a56074316c56c7d55224302f26a117be8f4
Max <burikovmaks@mail.ru>
2025-01-03 16:23:10 +0300

    Add photo for Donuts

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
+import os
+from flask import send_from_directory
 from app import create_app
 
+
 app = create_app()
+
+
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    """Сервировка изображений из папки images."""
+    images_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'images'))
+    return send_from_directory(images_dir, filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
